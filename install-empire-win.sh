410936bed2de8b9c9a8690cd9785e06a570df1e8
unknown <rbrockw@RBROCKWe6420.corp.bbns.com>
2015-11-11 14:02:19 -0500

    added /c/ to install path

--- install-empire-win.sh
+++ install-empire-win.sh
@@ -17,8 +17,8 @@ echo -e "\n ${BLUE}[*]${RESET} ${BOLD}Windows post-install script${RESET}\n"
 
 ##### Install Empire
 echo -e "\n ${GREEN}[+]${RESET} Installing ${GREEN}Empire${RESET} ~ post-exploitation framework"
-apt-get -y -qq install git || echo -e ' '${RED}'[!] Issue with apt-get'${RESET} 1>&2
-git clone -q https://github.com/powershellempire/empire/ /opt/empire-git/ || echo -e ' '${RED}'[!] Issue when git cloning'${RESET} 1>&2
-pushd /opt/empire-git/ >/dev/null
+#apt-get -y -qq install git || echo -e ' '${RED}'[!] Issue with apt-get'${RESET} 1>&2
+git clone -q https://github.com/powershellempire/empire/ /c/opt/empire-git/ || echo -e ' '${RED}'[!] Issue when git cloning'${RESET} 1>&2
+pushd /c/opt/empire-git/ >/dev/null
 git pull -q
 popd >/dev/null
