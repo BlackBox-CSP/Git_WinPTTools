50988ef889a50433864df6389ff801ef77003d56
unknown <rbrockw@RBROCKWe6420.corp.bbns.com>
2015-11-11 14:18:39 -0500

    added /c/ to paths

--- windows-post-install-script.sh
+++ windows-post-install-script.sh
@@ -31,56 +31,56 @@ echo -e "\n ${BLUE}[*]${RESET} ${BOLD}Windows post-install script${RESET}\n"
 #popd >/dev/null
 ##### Install Generate-Macro
 echo -e "\n ${GREEN}[+]${RESET} Installing ${GREEN}Generate-Macro${RESET} ~ malicious macro generator"
-git clone -q https://github.com/enigma0x3/Generate-Macro/ /opt/generate-macro-git/ || echo -e ' '${RED}'[!] Issue when git cloning'${RESET} 1>&2
-pushd /opt/generate-macro-git/ >/dev/null
+git clone -q https://github.com/enigma0x3/Generate-Macro/ /c/opt/generate-macro-git/ || echo -e ' '${RED}'[!] Issue when git cloning'${RESET} 1>&2
+pushd /c/opt/generate-macro-git/ >/dev/null
 git pull -q
 popd >/dev/null
 ##### Install Get-bADpasswords
 echo -e "\n ${GREEN}[+]${RESET} Installing ${GREEN}Get-bADpasswords${RESET} ~ PS password hash extrator and analyzer"
-git clone -q https://github.com/ZilentJack/Get-bADpasswords/ /opt/get-bADpasswords-git/ || echo -e ' '${RED}'[!] Issue when git cloning'${RESET} 1>&2
-pushd /opt/get-bADpasswords-git/ >/dev/null
+git clone -q https://github.com/ZilentJack/Get-bADpasswords/ /c/opt/get-bADpasswords-git/ || echo -e ' '${RED}'[!] Issue when git cloning'${RESET} 1>&2
+pushd /c/opt/get-bADpasswords-git/ >/dev/null
 git pull -q
 popd >/dev/null
 ##### Install Nishang
 echo -e "\n ${GREEN}[+]${RESET} Installing ${GREEN}Nishang${RESET} ~ post-exploitation framework"
-git clone -q https://github.com/samratashok/nishang/ /opt/nishang-git/ || echo -e ' '${RED}'[!] Issue when git cloning'${RESET} 1>&2
-pushd /opt/nishang-git/ >/dev/null
+git clone -q https://github.com/samratashok/nishang/ /c/opt/nishang-git/ || echo -e ' '${RED}'[!] Issue when git cloning'${RESET} 1>&2
+pushd /c/opt/nishang-git/ >/dev/null
 git pull -q
 popd >/dev/null
 ##### Install PoshSecMod
 echo -e "\n ${GREEN}[+]${RESET} Installing ${GREEN}PoshSecMod${RESET} ~ powershell cmdlets"
-git clone -q https://github.com/darkoperator/Posh-SecMod/ /opt/posh-secmod-git/ || echo -e ' '${RED}'[!] Issue when git cloning'${RESET} 1>&2
-pushd /opt/posh-secmod-git/ >/dev/null
+git clone -q https://github.com/darkoperator/Posh-SecMod/ /c/opt/posh-secmod-git/ || echo -e ' '${RED}'[!] Issue when git cloning'${RESET} 1>&2
+pushd /c/opt/posh-secmod-git/ >/dev/null
 git pull -q
 popd >/dev/null
 ##### Install Powercat
 echo -e "\n ${GREEN}[+]${RESET} Installing ${GREEN}Powercat${RESET} ~ powershell netcat"
-git clone -q https://github.com/besimorhino/powercat/ /opt/powercat-git/ || echo -e ' '${RED}'[!] Issue when git cloning'${RESET} 1>&2
-pushd /opt/powercat-git/ >/dev/null
+git clone -q https://github.com/besimorhino/powercat/ /c/opt/powercat-git/ || echo -e ' '${RED}'[!] Issue when git cloning'${RESET} 1>&2
+pushd /c/opt/powercat-git/ >/dev/null
 git pull -q
 popd >/dev/null
 ##### Install PowerShell-Scripts
 echo -e "\n ${GREEN}[+]${RESET} Installing ${GREEN}PowerShell-Scripts${RESET} ~ powershell attack scripts"
-git clone -q https://github.com/roo7break/PowerShell-Scripts/ /opt/powershell-scripts-git/ || echo -e ' '${RED}'[!] Issue when git cloning'${RESET} 1>&2
-pushd /opt/powershell-scripts-git/ >/dev/null
+git clone -q https://github.com/roo7break/PowerShell-Scripts/ /c/opt/powershell-scripts-git/ || echo -e ' '${RED}'[!] Issue when git cloning'${RESET} 1>&2
+pushd /c/opt/powershell-scripts-git/ >/dev/null
 git pull -q
 popd >/dev/null
 ##### Install PowerSploit
 echo -e "\n ${GREEN}[+]${RESET} Installing ${GREEN}PowerSploit${RESET} ~ powershell attack framework"
-git clone -q https://github.com/mattifestation/PowerSploit/ /opt/powersploit-git/ || echo -e ' '${RED}'[!] Issue when git cloning'${RESET} 1>&2
-pushd /opt/powersploit-git/ >/dev/null
+git clone -q https://github.com/mattifestation/PowerSploit/ /c/opt/powersploit-git/ || echo -e ' '${RED}'[!] Issue when git cloning'${RESET} 1>&2
+pushd /c/opt/powersploit-git/ >/dev/null
 git pull -q
 popd >/dev/null
 ##### Install PowerTools
 echo -e "\n ${GREEN}[+]${RESET} Installing ${GREEN}PowerTools${RESET} ~ powershell post-exploitaion framework"
-git clone -q https://github.com/PowerShellEmpire/PowerTools/ /opt/powertools-git/ || echo -e ' '${RED}'[!] Issue when git cloning'${RESET} 1>&2
-pushd /opt/powertools-git/ >/dev/null
+git clone -q https://github.com/PowerShellEmpire/PowerTools/ /c/opt/powertools-git/ || echo -e ' '${RED}'[!] Issue when git cloning'${RESET} 1>&2
+pushd /c/opt/powertools-git/ >/dev/null
 git pull -q
 popd >/dev/null
 ##### Install SprayWMI
 echo -e "\n ${GREEN}[+]${RESET} Installing ${GREEN}SprayWMI${RESET} ~ Windows WMI post-exploitation equivalent of PSEXEC"
-git clone -q https://github.com/trustedsec/spraywmi/ /opt/spraywmi-git/ || echo -e ' '${RED}'[!] Issue when git cloning'${RESET} 1>&2
-pushd /opt/spraywmi-git/ >/dev/null
+git clone -q https://github.com/trustedsec/spraywmi/ /c/opt/spraywmi-git/ || echo -e ' '${RED}'[!] Issue when git cloning'${RESET} 1>&2
+pushd /c/opt/spraywmi-git/ >/dev/null
 git pull -q
 popd >/dev/null
 ##### End of TSP adds
