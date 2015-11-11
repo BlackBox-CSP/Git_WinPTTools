de0aaa2ab4e9999d5591dbe92c14a0d9649d6d4f
unknown <rbrockw@RBROCKWe6420.corp.bbns.com>
2015-11-11 13:49:08 -0500

    initial version of install-empire-win.sh

---
+++ install-empire-win.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Discover installer
+# Last updated 11/10/2015
+
+##### (Cosmetic) Colour output
+RED="\033[01;31m"      # Issues/Errors
+GREEN="\033[01;32m"    # Success
+YELLOW="\033[01;33m"   # Warnings/Information
+BLUE="\033[01;34m"     # Heading
+BOLD="\033[01;01m"     # Highlight
+RESET="\033[00m"       # Normal
+
+
+echo -e "\n ${BLUE}[*]${RESET} ${BOLD}Windows post-install script${RESET}\n"
+
+#mkdir /opt/Windows/
+
+##### Install Empire
+echo -e "\n ${GREEN}[+]${RESET} Installing ${GREEN}Empire${RESET} ~ post-exploitation framework"
+apt-get -y -qq install git || echo -e ' '${RED}'[!] Issue with apt-get'${RESET} 1>&2
+git clone -q https://github.com/powershellempire/empire/ /opt/Windows/empire-git/ || echo -e ' '${RED}'[!] Issue when git cloning'${RESET} 1>&2
+pushd /opt/Windows/empire-git/ >/dev/null
+git pull -q
+popd >/dev/null
